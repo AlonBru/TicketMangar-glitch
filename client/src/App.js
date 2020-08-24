dc944602cc9ee453c2fe981de09e99c2c44d817b
AlonBru <AlonBru@users.noreply.github.com>
2020-08-24 20:44:14 +0300

    fixed search and hide

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 import logo from './logo.svg';
 import Ticket from './components/Ticket'
 import Search from './components/Search'
+import ShowButton from './components/ShowButton'
 import './App.css';
 
 function App() {
     const [ticketsToDisplay,setTicketsToDisplay]= useState(['loading'])
     const [ticketsHidden,setTicketsHidden]= useState(0)
-    const [query,setQuery] = setState('');
     
     useEffect( () => {
         grabTickets()   
@@ -16,38 +16,37 @@ function App() {
    async function grabTickets () {
        const tickets= (await axios.get('/api/tickets')).data;
        console.log('brought',tickets)
-       setTicketsHidden(ticketsHidden+1)
        setTicketsToDisplay(tickets)
     }
     function hideTicket(id){
         let newTickets = ticketsToDisplay.slice();
         let ticketToHide= newTickets.find(ticket=>ticket.id===id)
         ticketToHide.hide=true;
-
+        setTicketsHidden(ticketsHidden+1)
         setTicketsToDisplay(newTickets)
     }
     function unHideTickets(){
         let newTickets = ticketsToDisplay.map(ticket=>{
             if(ticket.hide){ticket.hide = false}
-            ticketToHide.hide=true;
-        }
+            return ticket
+        })
+        setTicketsToDisplay(newTickets);
+        setTicketsHidden(0);
+    }
+    async function  searchTickets(e){
+        let query = e.target.value;
+        console.log('searching', query)
+        let tickets = (await axios.get(`/api/tickets?searchText=${query}`)).data;
+        setTicketsToDisplay(tickets)
+    }
 
-        setTicketsToDisplay(newTickets)
-    }
-    function findLocal(e){
-        let query = e.target.value;
-        setQuery(query);
-    }
-    function searchTickets(e){
-        let query = e.target.value;
-        setQuery(query);
-    }
     if (ticketsToDisplay[0]==='loading'){
         return <h1>LOADING</h1>
     }else 
     return (
     <main>
-    <Search function={findLocal}/>
+    <ShowButton hidden={ticketsHidden} function={unHideTickets} />
+    <Search function={searchTickets}/>
     {ticketsToDisplay.map((ticket,index)=>{
         return <Ticket key={index} data={ticket} hide={hideTicket}/>
         })}
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Labels from './Label.js';
 
 const Search = (props) => {
     
     return(
-        <div className='ticket'>
-            <input className='searchBar' placeholder='search for a title or a part of one' onChange={props.function} />
-        </div>
+        <input id='searchInput' placeholder='search for a title or a part of one' onChange={props.function} />
         )
     }
 export default Search;

08be7dcd02ff3cddff2288e31795b1d7d1827d7a
AlonBru <AlonBru@users.noreply.github.com>
2020-08-25 15:59:03 +0300

    options sidebar
    added, WIP

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import Labels from './Label';
+// import FilterByLabel from './FilterByLabel';
 
 const Sidebar = (props) => {
-    const { search } = props; 
+    const { options,setOptions } = props; 
+    const { filterByLabel,hideClosed,timeRange } = options;
+    
+    function searchWithEnter(e){
+        if(e.key!=='Enter'){
+            return;
+        }
+        let {value} = e.target;
+        filterByLabel.push({name: value, active:true})
+        console.log(options.filterByLabel)
+        value='';
+        setOptions(options)
+        
+    }
+    function changeLabelFilter(e){
+        let { checked, id } = e.target
+        let labelToChange = filterByLabel.find(label=>label.name===id);
+        labelToChange.active = checked;
+        console.log(11,options)
+        setOptions(options)
+    }
     return(
-        <input id='searchInput' placeholder='search for a title or a part of one' onChange={search} />
+        <div id='sidebar'>
+            <label><input type='checkbox' />Show Closed</label>
+            <h2> Filter Tickets by label</h2>
+            <input placeholder='enter ticket label' onKeyDown={searchWithEnter} />
+           {filterByLabel.map(label=> 
+           {return(
+            <>
+            <input id={label.name} type='checkbox' checked={label.active} onChange={changeLabelFilter} />
+            <label htmlFor={label.name}>{label.name}</label>
+           </>
+           )}
+           )}
+           
+            
+        </div>
         )
     }
 export default Sidebar;

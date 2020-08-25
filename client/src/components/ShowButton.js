8892160713d06852ca130121daacbf3e086e06ad
AlonBru <AlonBru@users.noreply.github.com>
2020-08-25 15:58:07 +0300

    Update ShowButton.js
    variable naming

--- client/src/components/ShowButton.js
+++ client/src/components/ShowButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const ShowButton = (props) => {
-    if(props.hidden===0){return <></>}
+    const { hiddenTickets } =props
+    if(hiddenTickets===0){return <></>}
     return(
         <div className='showButton'>
-            <p><span id='hideTicketsCounter'>{props.hidden}</span> Tickets are hidden <button id='restoreHideTickets' onClick={props.function}>Show All Hidden</button></p>
+            <p><span id='hideTicketsCounter'>{hiddenTickets}</span> Tickets are hidden <button id='restoreHideTickets' onClick={props.function}>Show All Hidden</button></p>
             
         </div>
     )
